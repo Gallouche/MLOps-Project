2ab6e12ccca0bcfee667849bd0e40dc163e9b382
Gallouche <theo@monito.com>
2024-12-15 16:36:12 +0100

    add images and annotations upload

--- airflow/dags/preprocess.py
+++ airflow/dags/preprocess.py
@@ -1,12 +1,22 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+import os
 
-from src.gcs import download_and_delete_files_from_gcs
-from src.parse_annotations import convert_all_to_yolo
+from src.preprocessing.gcs_download import download_and_delete_files_from_gcs
+from src.preprocessing.parse_annotations import convert_all_to_yolo
+from src.preprocessing.gcs_upload import split_data, upload_new_data
 
-SERVICE_ACCOUNT_JSON = "./dags/drop-bucket-key.json"
+SERVICE_ACCOUNT_JSON_DOWNLOAD = "./dags/drop-bucket-key.json"
+SERVICE_ACCOUNT_JSON_UPLOAD = "./dags/mse-machledata-key.json"
 DATASET_FOLDER = "tmp/"
+
+
+# def list_files():
+#     for root, _, files in os.walk(DATASET_FOLDER):
+#         for file in files:
+#             print("File: ", os.path.join(root, file))
+
 
 # Define the DAG
 with DAG(
@@ -19,7 +29,7 @@ with DAG(
     download_and_delete_task = PythonOperator(
         task_id="download_and_delete_files",
         python_callable=download_and_delete_files_from_gcs,
-        op_kwargs={"SERVICE_ACCOUNT_JSON": SERVICE_ACCOUNT_JSON},
+        op_kwargs={"SERVICE_ACCOUNT_JSON": SERVICE_ACCOUNT_JSON_DOWNLOAD},
     )
 
     convert_all_to_yolo_task = PythonOperator(
@@ -29,5 +39,22 @@ with DAG(
                    "output_directory": DATASET_FOLDER + "annotations_yolo/"},
     )
 
+    list_files_task = PythonOperator(
+        task_id="list_files",
+        python_callable=list_files,
+    )
+
+    split_data_task = PythonOperator(
+        task_id="split_data",
+        python_callable=split_data,
+        op_kwargs={"local_folder": DATASET_FOLDER},
+    )
+
+    upload_new_data_task = PythonOperator(
+        task_id="upload_new_data",
+        python_callable=upload_new_data,
+        op_kwargs={"SERVICE_ACCOUNT_JSON": SERVICE_ACCOUNT_JSON_DOWNLOAD}
+    )
+
     # Set task dependencies
-    download_and_delete_task >> convert_all_to_yolo_task
+    download_and_delete_task >> convert_all_to_yolo_task >> split_data_task >> upload_new_data_task
